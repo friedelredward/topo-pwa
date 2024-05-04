5ab571a47ed4208d3f9bc6b6a684a0495fe11cf6
R Edward Friedel <redwardweb@gmail.com>
2024-05-04 16:37:54 +0200

    feat: added multiple moles + unittest + coverage fix

--- topo-pwa/src/app/game/game.component.spec.ts
+++ topo-pwa/src/app/game/game.component.spec.ts
@@ -32,6 +32,7 @@ describe('GameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
   it('should initialize with default values', () => {
     expect(component.username).toBeNull();
     expect(component.isGameRunning).toBeFalse();
@@ -39,77 +40,71 @@ describe('GameComponent', () => {
     expect(component.gameSpeedMs).toEqual(LEVEL_TO_MS[LevelSpeed.LOW]);
     expect(component.actualPoints).toEqual(0);
     expect(component.moles.length).toEqual(component.BOARD_CELLS);
-    expect(component.visibleMole).toBeUndefined();
+    expect(component.visibleMoles).toHaveSize(0);
     expect(component['intervalId']).toBeNull();
   });
 
   it('should start the game', () => {
-    spyOn(component, 'getNewVisibleMole').and.callThrough();
+    spyOn(component, 'getNewVisibleMoles').and.callThrough();
     spyOn<any>(component, 'setNewInterval').and.callThrough();
 
     component.startGame();
     expect(component.isGameRunning).toBeTrue();
-    expect(component.getNewVisibleMole).toHaveBeenCalledTimes(1);
+    expect(component.getNewVisibleMoles).toHaveBeenCalledTimes(1);
     expect(component['setNewInterval']).toHaveBeenCalledOnceWith(component.gameSpeedMs);
   });
 
-  it('should Set new visible mole different from undefined', () => {
-    const dummyMole= 0;
-    spyOn<any>(component, 'getRandomMole').and.returnValue(dummyMole + 1);
+  it('should update visibleMoles with new unique moles different from the old ones', () => {
+    const oldMoles = [0, 1];
+    const newMoles = [2, 3];
+    spyOn<any>(component, 'getRandomMoles').and.returnValue(newMoles);
 
-    component.getNewVisibleMole();
-    expect(component.visibleMole).toEqual(dummyMole + 1);
-    expect(component['getRandomMole']).toHaveBeenCalledOnceWith(undefined);
-  });
-
-  it('should Set new visible mole different from 0', () => {
-    const dummyMole= 0;
-    spyOn<any>(component, 'getRandomMole').and.returnValue(dummyMole + 1);
-
-    component.getNewVisibleMole(dummyMole);
-    expect(component.visibleMole).toEqual(dummyMole + 1);
-    expect(component['getRandomMole']).toHaveBeenCalledOnceWith(dummyMole);
-  });
-
-  it('should Set new visible mole different from any', () => {
-    const dummyMole= 1;
-    spyOn<any>(component, 'getRandomMole').and.callThrough();
-
-    component.getNewVisibleMole(dummyMole);
-    expect(component.visibleMole).not.toEqual(dummyMole );
-    expect(component['getRandomMole']).toHaveBeenCalled();
+    component.getNewVisibleMoles(oldMoles);
+    expect(component.visibleMoles).toEqual(newMoles);
+    expect(component['getRandomMoles']).toHaveBeenCalledWith(oldMoles);
   });
 
   it('should stop the game', () => {
       component.stopGame();
       expect(component.isGameRunning).toBeFalse();
-      expect(component.visibleMole).toBeUndefined();
+      expect(component.visibleMoles).toHaveSize(0);
       expect(component['intervalId']).toBeNull();
+  });
+
+  it('should stop the game clearing interval', () => {
+    spyOn(window, 'clearInterval').and.callThrough();
+    component['intervalId'] = 1 as unknown as number;
+
+    component.stopGame();
+    expect(component.isGameRunning).toBeFalse();
+    expect(component.visibleMoles).toHaveSize(0);
+    expect(window.clearInterval).toHaveBeenCalledOnceWith(component["intervalId"]);
   });
 
   it('should update points and show snackbar on mole hit', () => {
     component.username = "test";
     spyOn<any>(component, 'openSnackBar' );
     const initialPoints = component.actualPoints;
-    component.onMoleHit(true);
+    component.onMoleHit(true, 1);
     expect(component.actualPoints).toEqual(initialPoints + LEVEL_TO_PTS[component.actualLvl]);
     expect(component['openSnackBar']).toHaveBeenCalledTimes(1);
   });
 
   it('should not update points on miss', () => {
     const initialPoints = component.actualPoints;
-    component.onMoleHit(false);
+    component.onMoleHit(false, 1);
     expect(component.actualPoints).toEqual(initialPoints);
   });
 
   it('should return true when mole is visible', () => {
-    component.visibleMole = 0;
+    component.visibleMoles = [0,1];
     expect(component.isVisibleMole(0)).toBeTrue();
+    expect(component.isVisibleMole(1)).toBeTrue();
   });
 
   it('should return false when mole is not visible', () => {
-    component.visibleMole = 1;
-    expect(component.isVisibleMole(0)).toBeFalse();
+    component.visibleMoles = [0,1];
+    expect(component.isVisibleMole(2)).toBeFalse();
   });
 
   it('should change game speed and update actual level', () => {
@@ -117,5 +112,30 @@ describe('GameComponent', () => {
     component.onLvlSpeedChange(newLevel);
     expect(component.gameSpeedMs).toEqual(LEVEL_TO_MS[newLevel]);
     expect(component.actualLvl).toEqual(newLevel);
+  });
+
+  it('should open snackbar when mole is hit', () => {
+    component.username = "test";
+    spyOn(component['_snackBar'], 'open');
+
+    component.onMoleHit(true, 1);
+    expect(component['_snackBar'].open).toHaveBeenCalledWith(
+      `Good Job ${component.username}!`,
+      "",
+      { duration: component.NOTIFICATION_DURATION}
+    );
+  });
+
+  it('should set a new interval and clear the old one if it exists', () => {
+    const gameSpeed = 1000; // 1 second
+    spyOn(window, 'setInterval').and.callThrough();
+    spyOn(window, 'clearInterval');
+    component['intervalId'] = setInterval(() => {
+    }, 500) as unknown as number; // 0.5 second
+
+    component['setNewInterval'](gameSpeed);
+
+    expect(window.clearInterval).toHaveBeenCalledTimes(1);
+    expect(window.setInterval).toHaveBeenCalled();
   });
 });
