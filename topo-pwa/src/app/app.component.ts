7c3e1ac000c9bd1fbfd1727f1205cfde63da0b4e
R Edward Friedel <redwardweb@gmail.com>
2024-05-05 22:57:23 +0200

    feat: visual enhancements and versiun upgrade

--- topo-pwa/src/app/app.component.ts
+++ topo-pwa/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MatIconModule} from "@angular/material/icon";
-import {CustomIconsService} from "./shared/services/custom-icons.service";
 
 @Component({
   selector: 'app-root',
@@ -15,16 +14,10 @@ import {CustomIconsService} from "./shared/services/custom-icons.service";
       align-content: center;
       justify-content: center;
       align-items: center;
-      background-image: url("/assets/img/green-bg2.jpg");
-      background-repeat: no-repeat;
-      background-size: cover;
-      color: white;
     }
   `]
 })
 export class AppComponent {
-  constructor(private customIcons: CustomIconsService
-  ) {
-
+  constructor() {
   }
 }
